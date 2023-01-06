aa0bc0c09ed0f95cf8fc3fc29edce2a843f0a9ba
Ahmad Al Moustafa <almoustafa.ahmad@gmail.com>
2023-01-06 14:54:25 +0800

    Cart - close dropdown - fix

--- src/components/cart/CartDropdown.js
+++ src/components/cart/CartDropdown.js
@@ -1,10 +1,13 @@
 import { Link } from "react-router-dom";
 import CartDropdownItem from "./CartDropdownItem";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { selectCartItems } from "./../../features/cart/selector";
+import { setIsCartOpen } from "../../features/cart/cartSlice";
 
 const CartDropdown = () => {
   const cartItems = useSelector(selectCartItems);
+  const dispatch=useDispatch();
+
   return (
     <div className="absolute mt-2 top-12 right-2  w-1/5 bg-white px-10 py-10">
       <div className=" w-full text-black ">
@@ -15,7 +18,7 @@ const CartDropdown = () => {
 
       <Link to="/checkout" className=" w-4/5 ">
         <div className="border-t mt-1">
-          <button className=" w-full bg-indigo-500 font-semibold hover:bg-indigo-600 py-3 text-sm text-white uppercase w-full">
+          <button onClick={()=>dispatch(setIsCartOpen(false))} className=" w-full bg-indigo-500 font-semibold hover:bg-indigo-600 py-3 text-sm text-white uppercase w-full">
             Checkout
           </button>
         </div>
